8c7a21bd31988427e4454b459ab6775b3237d8f3
Ivan Suprun <60109954+Anderli-dev@users.noreply.github.com>
2025-01-27 08:17:09 +0200

    Added comments

--- configs/bot_config.py
+++ configs/bot_config.py
@@ -1,9 +1,17 @@
 from dotenv import load_dotenv
 import os
 
+# Define the path to the .env file located in the parent directory
+# This allows sensitive information like API keys to be loaded from a secure environment file
 env_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '.env')
 
+# Load environment variables from the specified .env file
 load_dotenv(dotenv_path=env_path)
 
+# Retrieve the bot token from environment variables
+# Used to authenticate with the Telegram API
 bot_token = os.getenv("BOT_TOKEN")
+
+# Retrieve the user ID from environment variables
+# Specifies the recipient of bot notifications
 user_id = os.getenv("TELEGRAM_USR_ID")
--- src/bot/handlers.py
+++ src/bot/handlers.py
@@ -1,13 +1,28 @@
 from aiogram import Bot
 
-from configs.bot_config import user_id, bot_token
+from configs.bot_config import user_id, bot_token  # Importing bot token and user ID from configuration
 
 
 async def send_message_to_user(text: str):
+    """
+    Asynchronously sends a message to a specific user using the Telegram Bot API.
+
+    Args:
+        text (str): The message content to send to the user.
+
+    Behavior:
+        - Connects to the Telegram bot using the provided bot token.
+        - Sends the message to the user with the specified user ID.
+        - Closes the bot session after the operation to release resources.
+    """
+    # Initialize the bot instance with the token from environment variables
     bot = Bot(token=bot_token)
     try:
-        await bot.send_message(chat_id=user_id, parse_mode="MarkdownV2",  text=text)
+        # Send a message to the specified user
+        await bot.send_message(chat_id=user_id, parse_mode="MarkdownV2", text=text)
     except Exception as e:
+        # Handle and print any errors that occur during the message-sending process
         print(f"Error: {e}")
     finally:
+        # Ensure the bot session is properly closed to free up resources
         await bot.session.close()
