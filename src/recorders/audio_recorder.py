8b5bc3b8f7a9f85c38d0004792326dcc7deb1f74
Ivan Suprun <60109954+Anderli-dev@users.noreply.github.com>
2025-02-11 09:38:50 +0200

    Created Audio recorder class with recording and saving audio

---
+++ src/recorders/audio_recorder.py
@@ -0,0 +1,38 @@
+import time
+import wave
+
+import numpy as np
+import sounddevice as sd
+
+
+class AudioRecorder:
+    def __init__(self):
+        self.audio_frames = []
+        self.sample_rate = 44100
+        self.chanels = 2
+        self.AUDIO_FILENAME = "audio_record.wav"
+        self.is_recording = True
+        
+    def _callback(self, indata, frames, time, status):
+            if status:
+                print(status)
+            self.audio_frames.append(indata.copy())
+    
+    def record_audio(self):
+        """Функція для запису звуку"""
+        with sd.InputStream(samplerate=self.sample_rate, channels=self.chanels, callback=self._callback):
+            print("Аудіозапис почався...")
+            while self.is_recording:
+                time.sleep(0.1)
+                
+    def save_audio(self):
+        """Збереження аудіо у файл"""
+        wf = wave.open(self.AUDIO_FILENAME, "wb")
+        wf.setnchannels(self.chanels)
+        wf.setsampwidth(2)  # 16-бітне аудіо
+        wf.setframerate(self.sample_rate)
+        wf.writeframes(b"".join([np.int16(frame * 32767).tobytes() for frame in self.audio_frames]))
+        wf.close()
+        
+    def record_stop(self):
+        self.is_recording = False
